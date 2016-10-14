b7da15c8efda222d8c879b5d6535bc13cbbbb807
Michael J. Stealey <michael.j.stealey@gmail.com>
2016-10-13 22:10:46 -0400

    add libxml2

--- 4.2.0-preview/docker-entrypoint.sh
+++ 4.2.0-preview/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # if LOCAL_UID and LOCAL_GID are not set, then run as root user
 if [[ -z "$LOCAL_UID" ]] && [[ -z "$LOCAL_GID" ]]; then
-    #gosu root iinit ${IRODS_PASSWORD}
+    gosu root iinit ${IRODS_PASSWORD}
     gosu root "$@"
 # if LOCAL_UID and LOCAL_GID are set, then run as irods user
 elif [[ ! -z "$LOCAL_UID" ]] || [[ ! -z "$LOCAL_GID" ]]; then
@@ -13,8 +13,8 @@ elif [[ ! -z "$LOCAL_UID" ]] || [[ ! -z "$LOCAL_GID" ]]; then
     if [[ ! -z "$LOCAL_GID" ]]; then
         gosu root groupmod -g ${LOCAL_GID} irods
     fi
-    #gosu irods iinit ${IRODS_PASSWORD}
     gosu root chown -R $(id -u irods):$(id -g irods) /var/lib/irods
+    gosu irods iinit ${IRODS_PASSWORD}
     gosu irods "$@"
 fi
 
